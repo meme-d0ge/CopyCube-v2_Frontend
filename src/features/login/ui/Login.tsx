a42dfaadc8e3de0cc2e58168676fe8bedb199037
meme-d0ge <MEME_D0GE@proton.me>
2025-04-01 19:49:55 +0200

    login and register features now work with CSR

--- src/features/login/ui/Login.tsx
+++ src/features/login/ui/Login.tsx
@@ -1,3 +1,4 @@
+'use client'
 import React from 'react';
 import {Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle} from "@/shared/ui/card";
 import {Input} from "@/shared/ui/input";
--- src/features/register/ui/Register.tsx
+++ src/features/register/ui/Register.tsx
@@ -1,3 +1,4 @@
+'use client'
 import React from 'react';
 import {Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle} from "@/shared/ui/card";
 import {Label} from "@/shared/ui/label";
